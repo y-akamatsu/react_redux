4cafcd34c2a82e52b3062a3c4a066b4d926be314
y-akamatsu <ak1985tk@gmail.com>
2019-02-16 19:15:33 +0900

    create webpack.config.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,26 @@
 const gulp = require("gulp");
-const watch = require("gulp-watch")
+const watch = require("gulp-watch");
+const plumber = require("gulp-plumber");
+const notify = require("gulp-notify");
 const browserSync = require("browser-sync").create();
+const webpackStream = require("webpack-stream");
+const webpackConfig = require("./webpack.config.js");
+
+gulp.task("compile", function () {
+  return gulp.src([
+    ".src/js/app.js",
+  ])
+    .pipe(plumber({ errorHandler: notify.onError("<%= error.message %>") }))
+    .pipe(webpackStream(webpackConfig), null, function (err, stats) {
+      if (stats.compilation.errors.length > 0) {
+        notify({
+          title: "webpack errpr",
+          message: stats.compilation.errors[0].error
+        });
+      }
+    })
+    .pipe(gulp.dest("js"))
+});
 
 gulp.task("browser-sync", function () {
   browserSync.init({
@@ -10,10 +30,13 @@ gulp.task("browser-sync", function () {
   });
 });
 
-gulp.task("watch", function(){
-  watch(["./**/*.html", "./js/**.*js"], function() {
+gulp.task("watch", function () {
+  watch(["./src/js/**/**.js"], function () {
+    gulp.setMaxListeners(["compile"]);
+  })
+  watch(["./**/*.html", "./js/**.*js"], function () {
     browserSync.reload();
   })
 })
 
-gulp.task("default", gulp.series(gulp.parallel("browser-sync", "watch")));
+gulp.task("default", gulp.series(gulp.parallel("compile", "browser-sync", "watch")));
---
+++ src/js/app.js
@@ -0,0 +1,48 @@
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="header">受信箱</header>
+     )
+  }
+}
+
+class Footer extends React.Component {
+  render(){
+    return (
+      <footer className="footer">2019 Copyright</footer>
+    )
+  }
+}
+class SideArea extends React.Component {
+  render() {
+    return (
+      <div className="side-area"></div>
+    )
+  }
+}
+class MainArea extends React.Component {
+  render() {
+    return (
+      <div className="main-area">
+      <Header />
+      <Footer />
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="wrap">
+        <SideArea />
+        <MainArea />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
