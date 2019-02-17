25eed53fa4a9706b2596d3654f1e44a7e15ce130
y-akamatsu <ak1985tk@gmail.com>
2019-02-17 14:25:28 +0900

    bild bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         exclude: /node_modules/, // 除外するファイル/ディレクトリ（正規表現可）
         loader: 'babel-loader', // 使用するloader
         query: {
-          presets: ['es2015', 'react']
+          presets: ['@babel/preset-env', '@babel/preset-react']
         }
       }
     ]
