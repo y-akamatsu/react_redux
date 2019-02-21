6290b2886eca1ba80ce097cb47051e06d10c2bdc
y-akamatsu <ak1985tk@gmail.com>
2019-02-21 19:16:43 +0900

    Introduced sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require("gulp");
 const watch = require("gulp-watch");
 const plumber = require("gulp-plumber");
 const notify = require("gulp-notify");
+const sass = require("gulp-sass");
 const browserSync = require("browser-sync").create();
 const webpackStream = require("webpack-stream");
 const webpackConfig = require("./webpack.config.js");
@@ -22,6 +23,15 @@ gulp.task("compile", function () {
     .pipe(gulp.dest("js"))
 });
 
+gulp.task("sass", function() {
+  return gulp.src("./src/sass/**/*.scss")
+  .pipe(plumber({ //gulp-pulbmerでerrorで処理を止めないようにする
+    errorHandler: notify.onError("Error: <%= error.message %>") //gulp-notifyでエラーの内容を表示させる
+  }))
+  .pipe(sass())//pipeでコンパイルの処理
+  .pipe(gulp.dest("./css"))//gulp.destでcssファイルを表示させる
+});
+
 gulp.task("browser-sync", function () {
   browserSync.init({
     server: {
@@ -33,10 +43,14 @@ gulp.task("browser-sync", function () {
 gulp.task("watch", function () {
   watch(["./src/js/**/**.js"], function () {
     gulp.setMaxListeners(["compile"]);
-  })
+  });
+
+  watch(["./src/sass/**/**.scss"], function () {
+    gulp.setMaxListeners(["sass"]);
+  });
   watch(["./**/*.html", "./js/**/*.js"], function () {
     browserSync.reload();
-  })
+  });
 })
 
-gulp.task("default", gulp.series(gulp.parallel("compile", "browser-sync", "watch")));
+gulp.task("default", gulp.series(gulp.parallel("compile",　"sass", "browser-sync", "watch")));
